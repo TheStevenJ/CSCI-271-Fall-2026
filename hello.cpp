6088b00d6e621f1a7cde86948dc2449d7b87630e
Steven Jimenez <thestevenjimenez@gmail.com>
2026-09-11 00:02:22 -0400

    Assigment 1 Hello World

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string x;
+
+    cout << "Hello World!" << endl;
+    cout << "What is your name?: ";
+    cin >> x;
+    cout << "Nice to meet you, " << x << "!" << endl;
+
+    return 0;
+}
