f3a0e5e32fe54ec879d4c4d89df3f88e174ac5f4
Steven Jimenez <thestevenjimenez@gmail.com>
2026-09-16 10:56:21 -0400

    added comments

--- Assigment-2/hello.cpp
+++ Assigment-2/hello.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    string x;
-    int y;
+    string x; //stores whatever value for x as a string
+    int y; //stroes a number as a value for y as a integer
 
-    cout << "Enter name: ";
-    cin >> x;
-    cout << "Enter age: ";
-    cin >> y;
-    cout << "Hello, I am " << x << "!" <<  " I am " << y << " years old!" << endl;
+    cout << "Enter name: "; //asking the user for their name
+    cin >> x; //saves that value as x
+    cout << "Enter age: "; //asking the user for their age 
+    cin >> y; //saves that value as y
+    cout << "Hello, I am " << x << "!" <<  " I am " << y << " years old." << endl; //outputs the values inputed by the user for x and y as a sentence.
 
-    return 0;
+    return 0; //stops the loop
 }
