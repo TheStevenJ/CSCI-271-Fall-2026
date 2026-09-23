75e5564d3e48a898dd8cb85d63b8204970a56f61
Steven Jimenez <thestevenjimenez@gmail.com>
2026-09-23 18:35:37 -0400

    Added comments

--- Assigment-3/temp-check.cpp
+++ Assigment-3/temp-check.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 
 int main() {
-    int temp;
-    cout << "Enter the Temperature: ";
-    cin >> temp;
+    int temp; //declares temp as a integer.
+    cout << "Enter the Temperature: "; //ask the user for the temperature.
+    cin >> temp; //stores the inputed value inside of temp.
 
-    if(temp <= 32) {
+    if(temp <= 32) { //checks if temp is lower then 32 to run "Freezing" as a string.
         cout << "Freezing" << endl;
-    } else if (temp <= 59) {
+    } else if (temp <= 59) { //checks if temp is lower then 60 but higher then 32 to run "Cold" as a string.
         cout <<  "Cold" << endl;
-    } else if (temp <= 75) {
+    } else if (temp <= 75) { //checks if temp is lower then 76 but higher then 59 to run "Mild" as a string.
         cout << "Mild" << endl;
-    } else {
+    } else { //if temp is higher then 76, "Hot" is then executed as a string.
         cout << "Hot" << endl;
     }
 
